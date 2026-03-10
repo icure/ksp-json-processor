cb2fdba06c616e4d90cb42babd0e3207bc6d8e44
aduchate <ad@icure.com>
2026-03-10 14:08:48 +0100

    Better way of getting github credentials from local.proerties

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,25 +1,34 @@
 import java.util.Properties
 
-val localProps = Properties().apply {
-    val propsFile = file("local.properties")
-    if (propsFile.exists()) {
-        propsFile.inputStream().use { load(it) }
-    }
-}
-
-fun loadProp(key: String): String? {
-    return System.getenv(key) ?: localProps.getProperty(key)
-}
-
-val githubUsername = loadProp("githubUsername")
-val githubPassword = loadProp("githubPassword")
-
 pluginManagement {
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
     }
+}
+
+@Suppress("UnstableApiUsage")
+fun getGithubCredentials(): Pair<String, String> {
+    fun getRootDir(p: Project): String =
+        if (p.parent == null) p.projectDir.absolutePath
+        else getRootDir(p.parent!!)
+
+    val propsFileName = "${layout.rootDirectory}/local.properties"
+    val propsFile = file(propsFileName)
+    val localProps = Properties().apply {
+        if (propsFile.exists()) {
+            propsFile.inputStream().use { load(it) }
+        }
+    }
+    fun loadProp(key: String): String? {
+        return localProps.getProperty(key) ?: System.getenv("ORG_GRADLE_PROJECT_$key")
+    }
+
+    val githubUser = loadProp("githubUsername") ?: throw IllegalStateException("Cannot load github username")
+    val githubPass = loadProp("githubPassword") ?: throw IllegalStateException("Cannot load github password")
+
+    return githubUser to githubPass
 }
 
 dependencyResolutionManagement {
@@ -29,6 +38,7 @@ dependencyResolutionManagement {
         maven {
             url = uri("https://maven.pkg.github.com/icure/charix")
             credentials {
+                val (githubUsername, githubPassword) = getGithubCredentials()
                 username = githubUsername
                 password = githubPassword
             }
